b5538dcb08cc30cc10dd86c508fcf8b53d2ba349
realcath <cathubpremium@gmail.com>
2026-03-14 18:21:51 -0300

    Update loader.lua

--- src/main/loader.lua
+++ src/main/loader.lua
@@ -2,12 +2,6 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
 local allowedPlaceIds = {
-    2753915549,
-    85211729168715,
-    4442272183,
-    79091703265657,
-    7449423635,
-    100117331123089,
     14890802310,
     74747090658891
 }
@@ -22,11 +16,7 @@ for _, id in ipairs(allowedPlaceIds) do
 end
 
 if isAllowed then
-    if game.PlaceId == 14890802310 or game.PlaceId == 74747090658891 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/realcath/lab/refs/heads/main/src/main/x/y/z/BizarreLineage.lua"))()
-    else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/realcath/lab/refs/heads/main/src/main/x/y/z/BloxFruits.lua"))()
-    end
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/realcath/lab/refs/heads/main/src/main/x/y/z/BizarreLineage.lua"))()
 else
     if LocalPlayer then
         LocalPlayer:Kick("Game is not supported")
