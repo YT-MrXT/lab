6fa2053ddb08269b7bdce359badd6a4bd026bb5c
realcath <cathubpremium@gmail.com>
2026-03-25 00:43:33 -0300

    Add new place IDs and corresponding scripts

--- src/main/loader.lua
+++ src/main/loader.lua
@@ -1,16 +1,16 @@
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
-local allowedPlaceIds = {
-    14890802310,
-    74747090658891,
-    77747658251236
-}
+
 local scripts = {
     [14890802310] = "https://raw.githubusercontent.com/realcath/lab/refs/heads/main/src/main/x/y/z/BizarreLineage.lua",
     [74747090658891] = "https://raw.githubusercontent.com/realcath/lab/refs/heads/main/src/main/x/y/z/BizarreLineage.lua",
     [77747658251236] = "https://raw.githubusercontent.com/realcath/lab/refs/heads/main/src/main/x/y/z/SailorPiece.lua",
+    [138368689293913] = "https://raw.githubusercontent.com/realcath/lab/refs/heads/main/src/main/x/y/z/SailorPiece.lua",
+    [96767841099256] = "https://raw.githubusercontent.com/realcath/lab/refs/heads/main/src/main/x/y/z/SailorPiece.lua",
 }
+
 local scriptUrl = scripts[game.PlaceId]
+
 if scriptUrl then
     loadstring(game:HttpGet(scriptUrl))()
 else
